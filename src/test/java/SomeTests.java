f963a9ecbb46fd52ea36a41b8791decf69c7eafd
destroy <plohoy@me.com>
2019-11-22 21:31:54 +0300

    initialisation

---
+++ src/test/java/SomeTests.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SomeTests {
+
+    public static void main(String[] args) {
+       /* open("http://market.yandex.ru");
+
+        $("#header-search").val("холодильник").pressEnter();
+        $(byTitle("Холодильник ATLANT ХМ 4208-000")).click();
+        $(withText("Холодильник")).should(exist);*/
+        open("https://www.e-katalog.ru/"); //открывакем я.маркет
+        $("#ek-search").val("холодильник").pressEnter(); //находим поле поиска, вводим холодильник, жмем enter
+        $(By.xpath("//a[@class='model-short-title no-u']")).click(); //находим первую ссылку в выдаче, кликаем
+        $(byText("Холодильник")).should(exist); //позитивный тест на слово "Холодильник"
+
+    }
+}
